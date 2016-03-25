cfce879474ae24031d0b07954d867b6d479684c5
Diego <diego.vid.eco@gmail.com>
2016-03-25 10:42:34 -0600

    preparado primera prueba

--- sass/cltvo-themes/test_gulp/nixtamal/nixtahead.php
+++
@@ -1,7 +0,0 @@
-<!DOCTYPE>
-<html>
-<head>
-	<title><?php echo isset($module_name) ? $module_name : 'untitled module' ?></title>
-	<link rel="stylesheet" href="../../css/mazorca.css">
-</head>
-<body>
